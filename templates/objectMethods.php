d7806e91d95ea32003aeafe0b79539b6cdabf888
Pavel B <p.bondarovich@gmail.com>
2016-07-11 19:09:53 +0700

    + relativeUrl

--- templates/objectMethods.php
+++ templates/objectMethods.php
@@ -8,6 +8,10 @@ public function get<?php print preg_replace('/Id$/', 'Url', $column->getPhpName(
     return $this-><?php print $column->getName() ?> ? $this->getImage<?php if(count($columns) > 1) print 'RelatedBy' . $column->getPhpName() ?>()->getUrl() : '';
 }
 
+public function get<?php print preg_replace('/Id$/', 'RelativeUrl', $column->getPhpName()) ?>(){
+    return $this-><?php print $column->getName() ?> ? $this->getImage<?php if(count($columns) > 1) print 'RelatedBy' . $column->getPhpName() ?>()->getRelativeUrl() : '';
+}
+
 
 <?php
     endforeach;
